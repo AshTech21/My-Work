a737b97b114031f736f25ba0ebaf0753ebae22cc
AshTech21 <ashwinkulkarni2102@gmail.com>
2026-05-03 14:42:08 +0530

    Add files via upload

---
+++ C Programming/FunctionDemo.c
@@ -0,0 +1,12 @@
+//Q.Write a program to print the message on Console by using a function
+#include<stdio.h>
+void Display()      //function body
+{   
+    prinft("Hello Programmers..\n");       //printig the Message on Console
+}
+
+int main()
+{
+    Display();      //function call
+    return 0;
+}
