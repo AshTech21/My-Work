f4b912407dfb8cd38a7c2c3f2d1d90ea2b08a797
AshTech21 <ashwinkulkarni2102@gmail.com>
2026-05-03 12:50:45 +0530

    Add files via upload

---
+++ C Programming/Hello.c
@@ -0,0 +1,9 @@
+//Q. Write a program to print the message "Hello Programmers on Console"
+#include<stdio.h>
+
+int main()
+{
+    printf("Hello programmers\n"); //printing message on console
+
+    return 0;
+}
